1a560b481d304f99b88106640da5adf6b2f735f5
antonymarg <66637640+antonymarg@users.noreply.github.com>
2021-03-03 21:44:57 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
-
 const app = express();
+
+require('dotenv').config()
+
 
 app.use(express.json())
 
 //Define Routes
 app.use('/',require('./routes/router'));
 
-app.listen(3000);
-console.log("Server started on port 3000")
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Listening to ${PORT}`);
+    console.log('Press Ctrl+C to quit.');
+  });
+
+
+
+
---
+++ database.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+
+var db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DATABASE
+});
+
+db.connect();
+
+module.exports = db; 
