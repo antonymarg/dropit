f5e31538f9b942e12d1de249ed386b39f5ed1c16
antonymarg <66637640+antonymarg@users.noreply.github.com>
2021-03-03 21:46:28 +0200

    Add files via upload

--- routes/router.js
+++ routes/router.js
@@ -1,24 +1,48 @@
 const express = require('express');
-const authController = require('../controllers/auth')
+const authController = require('../controllers/auth');
+const dataController = require('../controllers/data');
+const achController = require('../controllers/achievement');
+const jwt = require('jsonwebtoken');
+const db = require('../database')
+
+
+function authorizeToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (token == null) return res.sendStatus(401)
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user.userID;
+        next();
+    })
+}
 
 //Create Connection
 const router = express.Router();
 
 
-
-
-router.get('/',(req,res)=>{
-    res.send("Cute")
+router.get('/', (req, res) => {
+    res.sendStatus(200)
 });
 
-
-router.get('/test',(req,res)=>{
-    res.send("Test")
-});
-
-router.post('/auth/register',authController.register )
-router.post('/auth/login',authController.login)
+router.post('/auth/register', authController.register)
+router.post('/auth/login', authController.login)
+router.get('/auth/userData',authorizeToken,authController.userData)
 
 
+router.get('/goalOfTheDay/value', authorizeToken, dataController.goalVal)
+router.get('/goalOfTheDay/update', authorizeToken, dataController.goalUpdate)
+router.get('/dailyConsumption', authorizeToken, dataController.dailyConsumption)
 
-module.exports= router; 
+router.get('/dashboard', authorizeToken, dataController.dashboard)
+router.get('/dashboard/insert',authorizeToken, dataController.insertSensor)
+router.get('/sensorsOfUser',authorizeToken, dataController.sensorsOfUser)
+router.get('/measurementsOfTheDay',authorizeToken, dataController.measurementsOfTheDay)
+
+
+router.get('/leaderboard', authorizeToken, dataController.leaderboard)
+
+router.get('/points/update',achController.pointReward)
+
+module.exports = router; 
